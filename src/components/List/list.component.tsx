9fa0ddd0e32c596fbd3ce9a6a6c3502964ea393a
aabarata <andrebarata.eu@gmail.com>
2024-01-10 16:39:22 +0100

    Infinite scroll and virtualization using react-window libs

--- src/components/List/list.component.tsx
+++ src/components/List/list.component.tsx
@@ -1,57 +1,77 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Todo } from "../../@types/todo";
+import { FixedSizeList as VirtualizedList } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
 import styles from "./list.module.scss";
 
 const DEFAULT_NUMBER_OF_ITEMS = 100;
 
-function Item({ item }: { item: Todo }) {
-  return <li>{item.todo}</li>;
-}
+const Item = ({
+  data,
+  index,
+  style,
+}: {
+  data: Todo[];
+  index: number;
+  style: any;
+}) => {
+  return <li style={style}>{data[index].todo}</li>;
+};
 
 const List = () => {
   const [list, setList] = useState<Todo[]>([]);
   const [page, setPage] = useState<number>(0);
-  const listRef = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
   const fetchData = async () => {
-    const response = await fetch(
-      `https://dummyjson.com/todos?limit=${DEFAULT_NUMBER_OF_ITEMS}&skip=${
-        page * DEFAULT_NUMBER_OF_ITEMS
-      }`
-    );
-    const data = await response.json();
-    setList([...list, ...data.todos]);
+    if (hasMore && !isLoading) {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/todos?limit=${DEFAULT_NUMBER_OF_ITEMS}&skip=${
+          page * DEFAULT_NUMBER_OF_ITEMS
+        }`
+      );
+      const data = await response.json();
+      setList([...list, ...data.todos]);
+      if (data.limit) {
+        setPage(page + 1);
+      } else {
+        setHasMore(false);
+      }
+      setIsLoading(false);
+    }
   };
 
-  const handleScroll = () => {
-    const divRef = listRef.current;
-    const clientHeight = divRef?.clientHeight || 0;
-    const scrollHeight = divRef?.scrollHeight || 0;
-    const scrollTop = divRef?.scrollTop || 0;
-    if (scrollHeight - scrollTop <= clientHeight) {
-      setPage(page + 1);
-      fetchData();
-    }
+  const isItemLoaded = (index: number) => {
+    return index !== list.length - 1;
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    const divRef = listRef.current;
-    divRef?.addEventListener("scroll", handleScroll);
-    return () => divRef?.removeEventListener("scroll", handleScroll);
-  });
-
   return (
-    <div ref={listRef} className={styles.list}>
-      <ul>
-        {list.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
-      </ul>
-    </div>
+    <InfiniteLoader
+      isItemLoaded={isItemLoaded}
+      itemCount={list.length}
+      loadMoreItems={fetchData}
+    >
+      {({ onItemsRendered, ref }) => (
+        <VirtualizedList
+          className={styles.list}
+          height={300}
+          itemCount={list.length}
+          itemSize={20}
+          itemData={list}
+          onItemsRendered={onItemsRendered}
+          ref={ref}
+          width="100%"
+        >
+          {Item}
+        </VirtualizedList>
+      )}
+    </InfiniteLoader>
   );
 };
 
